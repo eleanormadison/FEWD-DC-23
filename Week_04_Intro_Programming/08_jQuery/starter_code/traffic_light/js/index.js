9c2bb285e921cde8372aa94d7d140bb18911c9b4
eleanormadison <eleanor_old@Eleanors-MacBook-Pro-2.local>
2016-04-12 18:48:03 -0400

    .

--- Week_04_Intro_Programming/08_jQuery/starter_code/traffic_light/js/index.js
+++ Week_04_Intro_Programming/08_jQuery/starter_code/traffic_light/js/index.js
@@ -1,1 +1,36 @@
+$(document).ready(function() {
+
 //Implement the red light using jQuery. Don't forget to add the script tags.
+//stop is red when stop button is clicked//
+
+/*thisif for loooooooooooong comments*/
+var stopButtonId = '#stopButton';
+
+
+
+$(stopButtonId).click(colorStopLight);
+$('#slowButton').click(colorSlowLight);
+$('#goButton').click(colorGoLight);
+
+
+function colorStopLight(){
+	$('#stopLight').css('backgroundColor','red');
+	$('#slowLight,#goLight').css('backgroundColor',"black");
+	
+}
+
+
+function colorSlowLight(){
+	$('#slowLight').css('backgroundColor','yellow');
+	$('#stopLight').css('backgroundColor','black');
+	$('#goLight').css('backgroundColor',"black");
+}
+
+
+function colorGoLight(){
+	$('#goLight').css('backgroundColor','green');
+	$('#stopLight').css('backgroundColor','black');
+	$('#slowLight').css('backgroundColor',"black");
+}
+
+});
